a8df315d5606812109a6634258b98140f15dd06f
Willian Alves <wba.alves@gmail.com>
2019-10-19 16:19:06 -0300

    create new about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="hero-body">
-    <div class="container has-text-centered">
-      <h1 class="title">
-        TownSQ Health check dashboard
-      </h1>
-      <h2 class="subtitle">
-        Made by: <a href="https://wbaalves.wixsite.com/codebox">Codebox</a> @
-        Integradora II PUCRS
-      </h2>
-      <div>
-        <p><strong>Stack used</strong></p>
-        <ul>
-          <li>VueJS</li>
-          <li>Quarkus</li>
-          <li>MongoDB</li>
-          <li>CloudAMQP</li>
-          <li>Auth0</li>
-        </ul>
+  <section class="hero is-fullheight-with-navbar">
+    <img class="hero-background" src="@/assets/bannertownsq.png" />
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          TownSQ - Health check dashboard
+        </h1>
+        <div>
+          <h2 class="subtitle">
+            Made by:
+            <a href="https://wbaalves.wixsite.com/codebox">Codebox</a> @
+            Integradora II PUCRS
+          </h2>
+        </div>
+        <div>
+          <p><strong>Stack used</strong></p>
+          <ul>
+            <li>VueJS</li>
+            <li>Quarkus</li>
+            <li>MongoDB</li>
+            <li>CloudAMQP</li>
+            <li>Auth0</li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
