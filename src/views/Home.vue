51c57551716cd095126dfebbe56bf3bc891af9e8
Willian Alves <wba.alves@gmail.com>
2019-10-19 15:01:13 -0300

    create homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>Home</div>
+  <div>
+    <section class="hero is-fullheight-with-navbar">
+      <img class="hero-background" src="@/assets/bannertownsq.png" />
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Welcome to TownSQ - Health check dashboard
+          </h1>
+          <div v-if="!$auth.isAuthenticated && !$auth.loading">
+            <h2 class="subtitle">
+              Please login to access the dashboard.
+            </h2>
+            <LoginButton />
+          </div>
+          <div v-if="$auth.isAuthenticated">
+            <h2 class="subtitle">
+              Go to <router-link to="/dashboard">dashboard</router-link> to
+              access the application.
+            </h2>
+            <LoginButton />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import LoginButton from "@/components/LoginButton";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    LoginButton
+  }
 };
 </script>
+
+<style scoped>
+.hero-background {
+  position: absolute;
+  object-fit: cover;
+  object-position: center center;
+  width: 100%;
+  height: 100%;
+}
+</style>
