5bd34454f68f9f6f4b6b003008217210ad284104
Willian Alves <wba.alves@gmail.com>
2019-10-27 22:31:26 -0300

    add conditionals to condos to render with right color

--- src/components/Condo.vue
+++ src/components/Condo.vue
@@ -1,14 +1,43 @@
 <template>
   <div>
-    <a>{{ condo.name }}</a>
+    <a v-if="condo.score >= good_score" class="green-background">{{
+      condo.name
+    }}</a>
+    <a v-else-if="condo.score >= 30" class="yellow-background">{{
+      condo.name
+    }}</a>
+    <a v-else class="red-background">{{ condo.name }}</a>
   </div>
 </template>
 
 <script>
 export default {
   name: "Condo",
-  props: ["condo"]
+  props: {
+    condo: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      good_score: 70,
+      bad_score: 30
+    };
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.green-background {
+  background-color: rgba(170, 255, 153, 0.26);
+}
+
+.yellow-background {
+  background-color: rgba(255, 253, 153, 0.26);
+}
+
+.red-background {
+  background-color: rgba(255, 153, 153, 0.26);
+}
+</style>
