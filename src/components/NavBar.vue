96cd0a2bd39530c5f2489def92a547e5e055df20
Willian Alves <wba.alves@gmail.com>
2019-11-03 02:23:35 -0300

    remove navbar burguer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,25 +8,8 @@
       <router-link to="/" class="navbar-item">
         <img src="@/assets/logo.png" />
       </router-link>
-      <a
-        role="button"
-        class="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbar-menu"
-        @click="isOpen = !isOpen"
-        v-bind:class="{ 'is-active': isOpen }"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
     </div>
-    <div
-      id="navbar-menu"
-      class="navbar-menu"
-      v-bind:class="{ 'is-active': isOpen }"
-    >
+    <div id="navbar-menu" class="navbar-menu">
       <div class="navbar-start">
         <router-link to="/" class="navbar-item">Home</router-link>
         <router-link
@@ -53,17 +36,12 @@
 <script>
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+
 export default {
   name: "NavBar",
-  data: () => {
-    return {
-      isOpen: false
-    };
-  },
   components: {
     LoginButton,
     LogoutButton
-  },
-  methods: {}
+  }
 };
 </script>
